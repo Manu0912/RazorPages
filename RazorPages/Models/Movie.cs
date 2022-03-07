bb867eceb5e23f83efd532ec8f9c819b5481bd2a
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-03-07 16:56:02 -0700

    add rating

--- RazorPages/Models/Movie.cs
+++ RazorPages/Models/Movie.cs
@@ -14,5 +14,7 @@ namespace RazorPages.Models
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; } = string.Empty;
+
     }
 }
