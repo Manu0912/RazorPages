2a231bbf5f243b1bcfcf21a15ceceba70d797281
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-03-07 11:34:50 -0700

    add movie model, context and connect to database

---
+++ RazorPages/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+using RazorPages.Models;
+
+namespace RazorPages.Pages.Movies
+{
+#pragma warning disable CS8618
+#pragma warning disable CS8604
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPages.Data.RazorPagesContext _context;
+
+        public IndexModel(RazorPages.Data.RazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+#pragma warning restore CS8618
+#pragma warning restore CS8604
+}
