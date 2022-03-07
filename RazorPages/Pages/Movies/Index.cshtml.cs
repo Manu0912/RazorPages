b17943fe877c616547ddb1b1e0c28402e6ef8e7d
Emanuel Hugo Cacheda <emanuelcacheda@hotmail.com>
2022-03-07 16:08:25 -0700

    search by name

--- RazorPages/Pages/Movies/Index.cshtml.cs
+++ RazorPages/Pages/Movies/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPages.Data;
 using RazorPages.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorPages.Pages.Movies
 {
@@ -23,10 +24,22 @@ namespace RazorPages.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 #pragma warning restore CS8618
